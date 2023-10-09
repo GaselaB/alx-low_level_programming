3774c321f551695bff756a29b770bbe07cd47064
GaselaB <gaselabongane@gmail.com>
2023-10-09 11:27:03 -0700

    0x0D. C - Preprocessor

--- 0-main.c
+++ 0-main.c
@@ -1,18 +1,17 @@
-#include "main.h"
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
 #include <stdio.h>
 
 /**
- * main - check the code.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char c;
+	int s;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+	s = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
 }
