03baf6a688e8b8d33ae54c9bf61533a254c5e2b4
GaselaB <gaselabongane@gmail.com>
2023-10-09 10:51:49 -0700

    0x0D. C - Preprocessor

--- 0-create_array.c
+++ 0-create_array.c
@@ -1,29 +1,28 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * create_array - creates an array of chars,
- * and initializes it with a specific char
- * @size: input size
- * @c: input character
+ * and initializes it with a specific char.
+ * @size: the size of the array
+ * @c: initial value
  *
  * Return: a pointer to the array, or NULL if it fails
  */
-
 char *create_array(unsigned int size, char c)
 {
-	unsigned int i = 0;
-	char *arr;
+	char *array;
+	unsigned int i;
 
-	arr = (char *) malloc(size);
-	if (size == 0 || arr == NULL)
-	{
+	if (size == 0)
 		return (NULL);
-	}
+
+	array = (char *)malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
-	{
-		arr[i] = c;
-	}
-	return (arr);
+		array[i] = c;
+
+	return (array);
 }
