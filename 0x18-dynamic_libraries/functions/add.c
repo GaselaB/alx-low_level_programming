c2673ba3bebfe276951e0c814d4b070a3efcad34
Bongane Gasela <gaselabongane@gmail.com>
2023-12-15 13:36:24 -0800

    task o

---
+++ 0x18-dynamic_libraries/functions/add.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdlib.h>
+
+/**
+ * add: add two integers
+ * @a: first integer
+ * @b: second integer
+ *Return: Addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x18-dynamic_libraries/functions/sub.c
+++ 0x18-dynamic_libraries/functions/sub.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 
 /**
- * add - add two numbers
+ * add- Subtract two numbers
  * @a: first number 
  * @b: second number
  * Return: aggregate result
  */
 int sub(int a, int b)
 {
-	return (a + b);
+	return (a - b);
 }
