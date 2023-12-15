89c978dad23327484b6d8fbdfe0034991fd99d8b
Bongane Gasela <gaselabongane@gmail.com>
2023-12-15 11:17:35 -0800

    task 0

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * mod - two numbers mod
+ * @a: first number 
+ * @b: second number
+ * Return: aggregate result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * sub - minus two numbers
+ * @a: first number 
+ * @b: second number
+ * Return: deducted end result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
